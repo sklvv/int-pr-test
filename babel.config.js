0e99cf5082d239be715cfebf7f45a88cf3f67440
ilya sokolov <ilya.socolov@yandex.ru>
2023-06-04 18:58:35 +0500

    chore: set up an alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			[
+				"module:react-native-dotenv",
+				{
+					envName: "APP_ENV",
+					moduleName: "@env",
+					path: ".env",
+				},
+			],
+			[
+				"module-resolver",
+				{
+					alias: {
+						"@": "./src/",
+					},
+				},
+			],
+		],
+	};
 };
